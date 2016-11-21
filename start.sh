3a8f797a3cc1bf7cb9c053213df1231cb303508b
andreafspeziale <andreaf.speziale@gmail.com>
2016-11-21 21:23:37 +0100

    param added

--- start.sh
+++ start.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
-production=false
-
+PRODUCTION=false
+DB_NAME='DATABASENAME'
+DB_USER='DATABASEUSER'
 # mysql setup
 if [ ! -f /setup.txt ]; then
   #mysql has to be started this way as it doesn't work to call from /etc/init.d
   /usr/bin/mysqld_safe &
   sleep 10s
 
-  if [ "$production" = true ] ; then
+  if [ "$PRODUCTION" = true ] ; then
       MYSQL_PASSWORD='ROOTPSW'
       APP_PASSWORD='APPPSW'
   else
@@ -18,7 +19,7 @@ if [ ! -f /setup.txt ]; then
 
   mysqladmin -u root password $MYSQL_PASSWORD
   mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE database_name; GRANT ALL PRIVILEGES ON database_name.* TO 'database_user'@'localhost' IDENTIFIED BY '$APP_PASSWORD'; FLUSH PRIVILEGES;"
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE '$DB_NAME'; GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$APP_PASSWORD'; FLUSH PRIVILEGES;"
   killall mysqld
 fi
 
