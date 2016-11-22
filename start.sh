a641f8de4dff6de15b31f9ea8f72ad889df1653a
andreafspeziale <andreaf.speziale@gmail.com>
2016-11-22 10:10:08 +0100

    fix on mysql setup script

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+!/bin/bash
 
 PRODUCTION=false
-DB_NAME='DATABASENAME'
-DB_USER='DATABASEUSER'
+DB_NAME=something
+DB_USER=something
 # mysql setup
 if [ ! -f /setup.txt ]; then
   #mysql has to be started this way as it doesn't work to call from /etc/init.d
@@ -10,16 +10,16 @@ if [ ! -f /setup.txt ]; then
   sleep 10s
 
   if [ "$PRODUCTION" = true ] ; then
-      MYSQL_PASSWORD='ROOTPSW'
-      APP_PASSWORD='APPPSW'
+      MYSQL_PASSWORD=ROOTPSW
+      APP_PASSWORD=APPPSW
   else
-      MYSQL_PASSWORD='LOCALROOTPSW'
-      APP_PASSWORD='LOCALAPPPSW'
+      MYSQL_PASSWORD=root
+      APP_PASSWORD=local
   fi
 
   mysqladmin -u root password $MYSQL_PASSWORD
   mysql -uroot -p$MYSQL_PASSWORD -e "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY '$MYSQL_PASSWORD' WITH GRANT OPTION; FLUSH PRIVILEGES;"
-  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE '$DB_NAME'; GRANT ALL PRIVILEGES ON '$DB_NAME'.* TO '$DB_USER'@'localhost' IDENTIFIED BY '$APP_PASSWORD'; FLUSH PRIVILEGES;"
+  mysql -uroot -p$MYSQL_PASSWORD -e "CREATE DATABASE $DB_NAME; GRANT ALL PRIVILEGES ON $DB_NAME.* TO $DB_USER@'localhost' IDENTIFIED BY '$APP_PASSWORD'; FLUSH PRIVILEGES;"
   killall mysqld
 fi
 
